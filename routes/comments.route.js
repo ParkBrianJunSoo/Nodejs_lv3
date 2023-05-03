1e69ab88c555f6bac5dc86c11578981bd758e367
ParkBrianJunSoo <junparkstar@gmail.com>
2023-05-03 22:27:54 +0900

    댓글 수정 기능

--- routes/comments.route.js
+++ routes/comments.route.js
@@ -33,7 +33,7 @@ router.post("/:postId/comments", authMiddleware, async (req, res) => {
 });
 
 
-// // 댓글 조회
+// 댓글 조회
 router.get('/:postId/comments', async (req, res) => {
     try {
         const { postId } = req.params;
@@ -59,6 +59,45 @@ router.get('/:postId/comments', async (req, res) => {
         });
     }
 });
+
+
+// 댓글 수정
+router.put('/:postId/comments/:commentId', authMiddleware, async (req, res) => {
+    try {
+        const { userId } = res.locals.user;
+        const { postId, commentId } = req.params;
+        const { comment } = req.body;
+
+        const getExistPost = await Posts.findOne({ where: { postId } });
+        const getExistComment = await Comments.findOne({ where: { commentId } });
+
+        if (!getExistPost) {
+            return res.status(404).json({ errorMessage: "게시글이 존재하지 않습니다." });
+        } else if (comment.length === 0) {
+            return res.status(400).json({ errorMessage: "데이터 형식이 올바르지 않습니다." });
+        } else if (!getExistComment) {
+            return res.status(404).json({ errorMessage: "댓글이 존재하지 않습니다." });
+        } else if (userId !== getExistComment.UserId) {
+            return res.status(403).json({ errorMessage: "댓글의 수정 권한이 존재하지 않습니다." });
+        }
+
+        await Comments.update(
+            { comment },
+            {
+                where: {
+                    [Op.and]: [{ commentId }, { PostId: postId }, { UserId: userId }]
+                }
+            }
+        );
+        return res.status(200).json({ message: "댓글을 수정하였습니다." });
+    } catch (err) {
+        console.err(`${req.method} ${req.originalUrl} : ${err.message}`);
+        return res.status(400).json({
+            errorMessage: '게시글 수정에 실패하였습니다.'
+        });
+    }
+});
+
 
 
 // 댓글 삭제
